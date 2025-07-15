d456ffac418d3b9da795ceb02c5cf065387f5c98
StefanB218 <bilekstefan@gmail.com>
2025-07-15 21:33:16 +0200

    add service worker

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,11 @@ import StartGame from './game';
 document.addEventListener('DOMContentLoaded', () => {
   StartGame('game-container');
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./src/service-worker.js').catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+  });
+}
