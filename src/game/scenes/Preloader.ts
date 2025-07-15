9425e1542f0639bf654a8c8321d7611983ade632
StefanB218 <bilekstefan@gmail.com>
2025-07-15 21:51:23 +0200

    Remove logo

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -20,7 +20,6 @@ export class Preloader extends Scene {
   preload() {
     //  Load the assets for the game - Replace with your own assets
     this.load.setPath('assets');
-    this.load.image('logo', 'logo.png');
 
     this.loadPlayerSpritesheet();
     this.loadParalaxBackground();
